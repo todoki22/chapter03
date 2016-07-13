ef4aa5cd22d18fd87fa49b16601100c92a330cb0
kickscar <kickscar@gmail.com>
2016-07-13 15:12:51 +0900

    1st commit

---
+++ src/chapter03/StringTest.java
@@ -0,0 +1,22 @@
+package chapter03;
+
+public class StringTest {
+
+	public static void main(String[] args) {
+		
+		//이스케이프 문자 사용
+		char c = '\'';
+		String s = "He\"llo\" \tWorld\n";
+		
+		System.out.println( s );
+		
+		
+		// + 연산
+		System.out.println( "Hello" + "World" + 2 );
+		StringBuffer sb = new StringBuffer( "Hello");
+		sb.append( "World");
+		sb.append( 2 );
+		
+	}
+
+}
